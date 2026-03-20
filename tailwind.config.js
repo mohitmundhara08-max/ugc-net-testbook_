81a5bf84545f7e1bfad8c81e564926c69b14625e
mohitmundhara08-max <mohitmundhara08@gmail.com>
2026-03-20 14:28:36 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,14 @@
-
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#002D5B',
+      },
+    },
+  },
+  plugins: [],
+}
